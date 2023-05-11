3f7bfd9a2f45a1d1e192d369bc1b42bbc29fc90e
ashat2005 <ashatkydyrov61@gmail.com>
2023-05-11 17:57:43 -0400

    dz3

--- apps/posts/serializer.py
+++ apps/posts/serializer.py
@@ -1,8 +1,8 @@
 from rest_framework import serializers
 from apps.posts.models import Post
 
+
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Post
+        model = Post 
         fields = "__all__"
-    
---
+++ apps/users/serializer.py
@@ -0,0 +1,42 @@
+from rest_framework import serializers
+
+from apps.users.models import User
+from apps.posts.serializer import PostSerializer
+
+
+class UserSerializer(serializers.ModelSerializer):
+    user_posts = PostSerializer(many=True, read_only=True)
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'first_name', 'last_name',
+                  'email', 'date_joined', 'phone_number', 'profile_image',
+                  'user_posts')
+        
+class UserRegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(
+        max_length=100, write_only=True
+    )
+    password2 = serializers.CharField(
+        max_length=100, write_only=True
+    )
+    class Meta:
+        model = User 
+        fields = ('username', 'first_name', 'last_name',
+                  'email', 'phone_number', 'profile_image',
+                  'password', 'password2')
+        
+    def create(self, validated_data):
+        if validated_data['password'] == validated_data['password2']:
+            user = User.objects.create(
+                username=validated_data['username'],
+                first_name=validated_data['first_name'],
+                last_name=validated_data['last_name'],
+                email=validated_data['email'],
+                phone_number=validated_data['phone_number'],
+                profile_image=validated_data['profile_image'],
+            )
+            user.set_password(validated_data['password2'])
+            user.save()
+            return user
+        else:
+            raise serializers.ValidationError("Пароли не совпадают")
